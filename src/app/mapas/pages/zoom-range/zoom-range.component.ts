ef99b772f6f72ebfdb3e51df08f26676fe8476d6
Dariel <gomez.b.dariel@gmail.com>
2023-11-25 09:50:19 -0600

    Obtain central location

--- src/app/mapas/pages/zoom-range/zoom-range.component.ts
+++ src/app/mapas/pages/zoom-range/zoom-range.component.ts
@@ -2,7 +2,7 @@ import {
   AfterViewInit,
   Component,
   ElementRef,
-  OnInit,
+  OnDestroy,
   ViewChild,
 } from '@angular/core';
 import * as mapboxgl from 'mapbox-gl';
@@ -29,12 +29,20 @@ import * as mapboxgl from 'mapbox-gl';
     `,
   ],
 })
-export class ZoomRangeComponent implements AfterViewInit {
+export class ZoomRangeComponent implements AfterViewInit, OnDestroy {
   @ViewChild('mapa') divMapa!: ElementRef;
   mapa!: mapboxgl.Map;
   zoomLevel: number = 10;
+  center: [number, number] = [-99.16783609940862, 19.427514936366155]; // *Angel de la independencia
 
   constructor() {}
+
+  // * OnDestroy para destruir los EventListeners
+  ngOnDestroy(): void {
+    this.mapa.off('zoom', () => {});
+    this.mapa.off('zoomend', () => {});
+    this.mapa.off('move', () => {});
+  }
 
   ngAfterViewInit(): void {
     // console.log('afterViewInit', this.divMapa);
@@ -42,19 +50,26 @@ export class ZoomRangeComponent implements AfterViewInit {
     this.mapa = new mapboxgl.Map({
       container: this.divMapa.nativeElement,
       style: 'mapbox://styles/mapbox/streets-v11',
-      center: [-99.16783609940862, 19.427514936366155], // *Angel de la independencia
+      center: this.center,
       zoom: this.zoomLevel,
     });
 
     // *EventListener para obtener el Zoom Level
-    this.mapa.on('zoom', (ev) => {
+    this.mapa.on('zoom', (event) => {
       this.zoomLevel = this.mapa.getZoom();
     });
 
-    this.mapa.on('zoom', (ev) => {
+    this.mapa.on('zoomend', (event) => {
       if (this.mapa.getZoom() > 18) {
         this.mapa.zoomTo(18);
       }
+    });
+
+    // * Movimiento del mapa y adjunto a las coordenadas
+    this.mapa.on('move', (event) => {
+      const target = event.target;
+      const { lng, lat } = target.getCenter();
+      this.center = [lng, lat];
     });
   }
 
