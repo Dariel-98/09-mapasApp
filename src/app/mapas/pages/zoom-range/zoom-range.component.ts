d694597b7362f951a6a43f10020c7660c3b35379
Dariel <gomez.b.dariel@gmail.com>
2023-11-25 08:31:55 -0600

    Zoom Range

--- src/app/mapas/pages/zoom-range/zoom-range.component.ts
+++ src/app/mapas/pages/zoom-range/zoom-range.component.ts
@@ -1,11 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as mapboxgl from 'mapbox-gl';
 
 @Component({
   selector: 'app-zoom-range',
   templateUrl: './zoom-range.component.html',
   styles: [
-  ]
+    `
+      .mapa-container {
+        width: 100%;
+        height: 100%;
+      }
+      .row {
+        background-color: white;
+        border-radius: 5px;
+        bottom: 50px;
+        left: 50px;
+        padding: 10px;
+        position: fixed;
+        z-index: 999;
+      }
+    `,
+  ],
 })
-export class ZoomRangeComponent {
-
+export class ZoomRangeComponent implements OnInit {
+  ngOnInit(): void {
+    var map = new mapboxgl.Map({
+      container: 'mapa',
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: [-99.16783609940862, 19.427514936366155], //Angel de la independencia
+      zoom: 14,
+    });
+  }
 }
