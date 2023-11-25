7b5be6792849831aaef0627be74b1c960ab51a49
Dariel <gomez.b.dariel@gmail.com>
2023-11-25 07:01:58 -0600

    Setting up and app menu

---
+++ src/app/mapas/components/mini-mapas/mini-mapas.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mini-mapas',
+  templateUrl: './mini-mapas.component.html',
+  styles: [
+  ]
+})
+export class MiniMapasComponent {
+
+}
