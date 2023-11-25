9334eeeb0d47bd5093e22be63111c894899a00eb
Dariel <gomez.b.dariel@gmail.com>
2023-11-25 09:31:49 -0600

    Control Zoom level

--- src/app/mapas/pages/zoom-range/zoom-range.component.ts
+++ src/app/mapas/pages/zoom-range/zoom-range.component.ts
@@ -24,6 +24,7 @@ import * as mapboxgl from 'mapbox-gl';
         padding: 10px;
         position: fixed;
         z-index: 999;
+        width: 400px;
       }
     `,
   ],
@@ -31,17 +32,29 @@ import * as mapboxgl from 'mapbox-gl';
 export class ZoomRangeComponent implements AfterViewInit {
   @ViewChild('mapa') divMapa!: ElementRef;
   mapa!: mapboxgl.Map;
+  zoomLevel: number = 10;
 
   constructor() {}
 
   ngAfterViewInit(): void {
-    console.log('afterViewInit', this.divMapa);
+    // console.log('afterViewInit', this.divMapa);
 
     this.mapa = new mapboxgl.Map({
       container: this.divMapa.nativeElement,
       style: 'mapbox://styles/mapbox/streets-v11',
-      center: [-99.16783609940862, 19.427514936366155], //Angel de la independencia
-      zoom: 14,
+      center: [-99.16783609940862, 19.427514936366155], // *Angel de la independencia
+      zoom: this.zoomLevel,
+    });
+
+    // *EventListener para obtener el Zoom Level
+    this.mapa.on('zoom', (ev) => {
+      this.zoomLevel = this.mapa.getZoom();
+    });
+
+    this.mapa.on('zoom', (ev) => {
+      if (this.mapa.getZoom() > 18) {
+        this.mapa.zoomTo(18);
+      }
     });
   }
 
@@ -51,5 +64,9 @@ export class ZoomRangeComponent implements AfterViewInit {
 
   zoomOut() {
     this.mapa.zoomOut();
+  }
+
+  zoomCambio(valor: string) {
+    this.mapa.zoomTo(Number(valor));
   }
 }
